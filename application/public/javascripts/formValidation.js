f3b7a5237b2d2de07b5c6e2f7622f8ef1a2104e1
Jacky Qiu <jackyqiu@Jackys-MacBook-Air.local>
2020-11-22 21:49:54 -0800

    milestone2 update

--- application/public/javascripts/formValidation.js
+++ application/public/javascripts/formValidation.js
@@ -102,3 +102,4 @@ function passwordValidation() {
 
 userNameValidation();
 passwordValidation();
+
--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+router.get('/', function (req, res, next) {
+  res.render("index");
 });
 
+router.get("/home", (req, res, next) => {
+  res.render("home", { title: "home" });
+});
+
+router.get("/imagePost", (req, res, next) => {
+  res.render("imagePost", { title: "imagePost" });
+});
+
+router.get("/postImage", (req, res, next) => {
+  res.render("postImage", { title: "postImage" });
+});
+router.get("/register", (req, res, next) => {
+  res.render("register", { title: "register" });
+});
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "login" });
+});
 module.exports = router;
