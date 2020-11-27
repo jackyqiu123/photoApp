e1026801d1eabe897313ee4edf2b87b09418ab4c
Jacky Qiu <jackyqiu@Jackys-MacBook-Air.local>
2020-11-26 21:56:04 -0800

    implemented user login authenticity/ added nav bar for home gallery page

--- application/routes/users.js
+++ application/routes/users.js
@@ -21,45 +21,54 @@ router.post('/register', (req, res, next) => {
   else if (password == "" || username == "" || email == "" || confirmPw == "") {
     res.redirect('/register');
   }
-  // else {
-  //   res.render('login');
-  // }
-  db.execute("SELECT * FROM users WHERE username = ?", [username], (err, results, fields) => {
-    var count = 0;
-    if (results && results.length == 0) { // nonexistent username
-      count++;
-    }
-    db.execute("SELECT * FROM users WHERE email = ?", [email], (err, results, fields) => {
-      if (results && results.length == 0) { // nonexistent email
+  else {
+    db.execute("SELECT * FROM users WHERE username = ?", [username], (err, results, fields) => {
+      var count = 0;
+      if (results && results.length == 0) { // nonexistent username
         count++;
       }
-      if (count == 2) { // if count = 2 means that both username and email is not taken 
-        let insertNewUser = "INSERT INTO users (username, email, password, created) VALUES(?, ?, ?, now());";
-        db.execute(insertNewUser, [username, email, password]);
-        res.redirect('/login');
-      }
-      else { // if either username and email is taken, then registration is not going to be sucessful
-        res.redirect('/register');
-        console.log("username or email is already taken");
-      }
+      db.execute("SELECT * FROM users WHERE email = ?", [email], (err, results, fields) => {
+        if (results && results.length == 0) { // nonexistent email
+          count++;
+        }
+        if (count == 2) { // if count = 2 means that both username and email is not taken 
+          let insertNewUser = "INSERT INTO users (username, email, password, created) VALUES(?, ?, ?, now());";
+          db.execute(insertNewUser, [username, email, password]);
+          res.redirect('/login');
+        }
+        else { // if either username and email is taken, then registration is not going to be sucessful
+          res.redirect('/register');
+          console.log("username or email is already taken");
+        }
+      })
+
     })
-
-  })
-
+  }
 });
 
-
 router.post('/login', (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
   if (req.body.username == "" || req.body.password == "") {
     res.redirect('/login');
   }
-  db.query('SELECT * FROM users', (err, results, fields) => {
-    results.forEach(user => {
-      if (user.username == req.body.username && user.password == req.body.password) {
-        console.log("login success");
+  db.execute("SELECT * FROM users WHERE username = ?", [username], (err, results, fields) => {
+    if (results && results.length > 0) { // checks if there's a matched username in the database
+      // console.log(results[0].password);
+      let matchedUserPw = results[0].password;
+      if (matchedUserPw == password) {
+        console.log(`${username} has successfully logged in`);
+        res.redirect("/homeGallery");
       }
-    })
-    res.redirect('/login');
-  })
-});
+      else {
+        console.log("username or password is incorrect");
+        res.redirect("/login");
+      }
+    }
+    else {
+      console.log("username or password is incorrect");
+      res.redirect("/login");
+    }
+  });
+})
 module.exports = router;
